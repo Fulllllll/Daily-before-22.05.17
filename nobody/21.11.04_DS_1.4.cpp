71849fa779955ebb363b30c2307fde7db9184427
Fulllllll <1476839330@qq.com>
2021-11-06 13:02:18 +0800

    21.11.06

--- nobody/21.11.04_DS_1.4.cpp
+++ nobody/21.11.04_DS_1.4.cpp
@@ -19,10 +19,13 @@ bool PrintList(LinkList L);
 bool SQLinkList(LinkList& L); 
 int compare(const void*e1,const void* e2);
 bool Sort(element* arr,int len); 
+bool Subtraction(const LinkList& A ,const LinkList& B,LinkList& C); 
+bool Intersection(const LinkList& A ,const LinkList& B,LinkList& C);
+bool Union(const LinkList& A ,const LinkList& B,LinkList& C); 
 
 int main()
 {
-	LinkList A,B;
+	LinkList A , B;
 	InitList(A);
 	InitList(B);
 	//给集合 A 赋值 
@@ -31,15 +34,34 @@ int main()
 	InsTail(B,'f');InsTail(B,'h');InsTail(B,'b');InsTail(B,'g');InsTail(B,'d');InsTail(B,'a');
 	printf("原集合A：");PrintList(A);
 	printf("\n原集合B：");PrintList(B);
+	//排序 
 	SQLinkList(A);
 	SQLinkList(B);
 	printf("\n有序集合A：");PrintList(A);
 	printf("\n有序集合B：");PrintList(B);
+	//并集
+	LinkList C;
+	InitList(C);
+	Union(A,B,C);
+	printf("\n\nA与B的并：");PrintList(C);
+	DestoyList(C);
+	//交集
+	LinkList D;
+	InitList(D);
+	Intersection(A,B,D);
+	printf("\nA与B的交：");PrintList(D);
+	DestoyList(D);
+	//差集
+	LinkList E;
+	InitList(E);
+	Subtraction(A,B,E);
+	printf("\nA与B的差：");PrintList(E);
+	DestoyList(E);
+	
+	DestoyList(A);
+	DestoyList(B);
+	
 }
-
-
-
-
 
 
 
@@ -131,6 +153,61 @@ bool SQLinkList(LinkList& L)
 	return 1;
 }
 
+bool Subtraction(const LinkList& A ,const LinkList& B,LinkList& C)
+{
+	Link p1=A.head->next;
+	while(p1)
+	{
+		Link p2=B.head->next;
+		while(p2)
+		{
+			if(p2->ch==p1->ch) break;
+			p2=p2->next;		
+		}		
+		if(!p2) InsTail(C,p1->ch);
+		p1=p1->next;
+	}
+	SQLinkList(C);
+	return 1;
+}
+bool Intersection(const LinkList& A ,const LinkList& B,LinkList& C)
+{
+	Link p1=A.head->next;
+	while(p1)
+	{
+		Link p2=B.head->next;
+		while(p2)
+		{
+			if(p2->ch==p1->ch) break;
+			p2=p2->next;		
+		}		
+		if(p2) InsTail(C,p1->ch);
+		p1=p1->next;
+	}
+	SQLinkList(C);
+	return 1;
+}
+
+bool Union(const LinkList& A ,const LinkList& B,LinkList& C)
+{
+	LinkList T;
+	InitList(T);
+	Subtraction(B,A,T);
+	Link p1=A.head->next;
+	while(p1)
+	{
+		InsTail(C,p1->ch);
+		p1=p1->next;	
+	}
+	Link p2=T.head->next;
+	while(p2)
+	{
+		InsTail(C,p2->ch);
+		p2=p2->next;
+	}
+	DestoyList(T);
+	SQLinkList(C);
+}
 
 
 
