8fea53675f3a4684c04bd53388e830b7330685b7
Fulllllll <1476839330@qq.com>
2021-11-15 20:22:32 +0800

    21.11.15

--- nobody/21.10.27.c
+++ nobody/21.10.27.c
@@ -1,94 +1,96 @@
-#include<bits/stdc++.h>
-//#define MAX 50
-using namespace std;
+#define STACK_INIT_SIZE	100
+#define STACK_INCREAMENT 10
+#include <stdio.h>
+#include <iostream>
+using naemspace std;
+
+typedef char SElemType;
+typedef struct {
+	SElemType* base;
+	SElemType* top;
+	int stacksize;
+}Sqstack;
+
+bool InitStack(Sqstack& S);
+bool DestroyStack(Sqstack& S);
+bool Display(Sqstack S);
+bool Isempty(Sqstack S);
+bool Push(Sqstack& S);
+bool Pop(Sqstack& S);
 
 
-void function(int n)
-{
-	if (n >= 10) function(n / 10);
-	printf("%d", n % 10);
-}
-//void function(int n)
-//{
-//	if (n >= 10) function(n / 10);
-//	printf("%d", n % 10);
-//}
 int main()
 {
-	function(5);
+	Sqstack S;
+	if (!Isempty) cout << "栈为空" << endl;
+
+		return 0;
+}
+
+bool InitStack(Sqstack& S)
+{
+	S.base = (SElemType*)malloc(STACK_INIT_SIZE * sizeof(SElemType));
+	if (!S) exit(-1);
+	S.top = S.base;
+	S.stacksize = STACK_INIT_SIZE;
+	return 1;
+}
+bool DestroyStack(Sqstack& S)
+{
+	S.base = NULL;
+	S.top = NULL;
+	free(S.top);
+	free(S.base);
+	Stacksize = 0;
+	return 1;
+}
+bool Display(Sqstack S)
+{
+	for (int i = 0; i < S.stacksize; i++)
+	{
+		cout << *(S.base + i) << "  ";
+	}
+	return 1;
+}
+
+bool Isempty(Sqstack S)
+{
+	if (S.stacksize == 0)return 1;
+	else return 0;
+}
+bool Push(Sqstack& S, SElemType e)
+{
+	if (S.top >= S.base + S.stacksize)
+	{
+		S.base = (SElemType*)realloc(S.base, (S.stacksize + STACK_INCREAMENT) * sizeof(SElemType));
+		if (!S.base) exit(-1);
+		S.top = S.base + S.stacksize;
+		S.stacksize += STACK_INCREAMENT;
+	}
+	*(S.top++) = e;
+	return 1;
+}
+bool Pop(Sqstack& S, SElemType& e)
+{
+	if (S.stacksize == 0)
+	{
+		cout << "栈空,pop失败";
+		exit(-1);
+	}
+	return 1;
 }
 
 
 
 
 
-//typedef int element;
-//typedef struct {
-//	element* elem;
-//	int lenth;
-//	int listsize;
-//}SqList;
-//void initRandomize(int* arr, int n, int min, int max);
-//bool InitList(SqList& L);
-//void ListPrint(SqList L);
-//void ListInsert(SqList& L, char e, int i);
-//void function(SqList& L);
-//
-//int main()
-//{
-//	SqList L;
-//	InitList(L);
-//	cout << "随机生产顺序表如下：\n";
-//	ListPrint(L);
-//	cout << "消除后重复数据的顺序表如下：\n";
-//	function(L);
-//	ListPrint(L);
-//}
-//
-//bool InitList(SqList& L)
-//{
-//	L.elem = (int*)malloc(MAX * sizeof(int));
-//	if (!L.elem) { cout << "栈溢出" << endl; exit(-1); }
-//	int n = 0;
-//	cout << "输入原顺序表长度:";
-//	cin >> n;
-//	L.lenth = n;
-//	L.listsize = MAX;
-//	initRandomize(L.elem, n, 1, 6);
-//}
-//
-//void function(SqList& L)
-//{
-//	element a[L.lenth];
-//	int count = 0;
-//	int i = 0, j = 0;
-//	for (i = 0; i < L.lenth; i++)
-//	{
-//		for (j = 0; j < i; j++)
-//		{
-//			if (L.elem[i] == L.elem[j]) break;
-//		}
-//		if (j >= i) a[count++] = L.elem[i];
-//	}
-//	L.elem = a;
-//	L.lenth = count;
-//}
-//
-//
-//void ListPrint(SqList L)
-//{
-//	for (int i = 0; i < L.lenth; i++)
-//	{
-//		cout << L.elem[i] << " ";
-//	}
-//	cout << endl;
-//}
-//void initRandomize(int* arr, int n, int min, int max)
-//{
-//	int i = 0;
-//	srand(time(0));  			/*设置种子,并生成伪随机序列*/
-//	for (i = 0; i < n; ++i) {
-//		arr[i] = rand() % (max - min + 1) + min;  /*得到从[min, max]之间的随机数*/
-////        printf("%d ", arr[i]);
-//	}
-//}
+
+
+
+
+
+
+
+
+
+
